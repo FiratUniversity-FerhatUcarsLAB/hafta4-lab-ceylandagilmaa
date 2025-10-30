3e465817a2b3e8df6dd6393ee2856315e775e552
ceylandagilmaa <dagilmaceylan@gmail.com>
2025-10-30 20:15:23 +0300

    Create Maaş.hesap.java

---
+++ Maaş.hesap.java
@@ -0,0 +1,9 @@
+--- Çalışan Bilgileri ---
+Adı      : Ceylan Dağılma
+Brüt Maaş: 80000 TL
+Haftalık Çalışma Saati: 40 saat
+Mesai Saati: 14 saat
+Mesai Ücreti: 300.00 TL/saat
+Toplam Mesai Ücreti: 4200.00 TL
+Vergi (%15): 12000.00 TL
+Net Maaş : 76200.00 TL
