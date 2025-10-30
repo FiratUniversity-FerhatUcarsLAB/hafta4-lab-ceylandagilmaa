0dfe79ce616046f47c16ada323c0fcb8b9e1df78
ceylandagilmaa <dagilmaceylan@gmail.com>
2025-10-30 19:35:57 +0300

    Öğrencinin adını girin: Ceylan Öğrencinin soyadını girin: Dağılma Öğrencinin numarasını girin: 250541108 Öğrencinin yaşını girin: 18  --- Öğrenci Bilgileri --- Adı        : Ceylan Soyadı     : Dağılma Numarası   : 250541108 Yaşı       : 18

---
+++ Öğrenci.Bilgi.java
@@ -0,0 +1,10 @@
+Öğrencinin adını girin: Ceylan
+Öğrencinin soyadını girin: Dağılma
+Öğrencinin numarasını girin: 250541108
+Öğrencinin yaşını girin: 18
+
+--- Öğrenci Bilgileri ---
+Adı        : Ceylan
+Soyadı     : Dağılma
+Numarası   : 250541108
+Yaşı       : 18
